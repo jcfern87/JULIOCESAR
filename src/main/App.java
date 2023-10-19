b85c14659265470d03a4bf65028e27ad09456b62
jcfern87 <136532734+jcfern87@users.noreply.github.com>
2023-10-19 20:27:26 +0000

    2 first tests completed

--- src/main/App.java
+++ src/main/App.java
@@ -1,11 +1,25 @@
 package main;
 public class App {
     public static int encontre(int[] arrayOrdenado, int num){
-        return -1;
+        for(int i = 0; i<arrayOrdenado.length; i++){
+            if(arrayOrdenado[i] == num){
+                return 1;
+            }
+            if(arrayOrdenado[i] >= num){
+                return 0;
+            }
+        }
+        return 0;
     }
 
     public static int maior(int[] arrayDesordenado){
-        return -1;
+        int maior = arrayDesordenado[0];
+        for(int i = 0; i<arrayDesordenado.length;i++){
+            if(arrayDesordenado[i] > maior){
+                maior = arrayDesordenado[i];
+            }
+        }
+        return maior;
     }
 
     public static Integer parMaisProximo(Integer[][] pares){
